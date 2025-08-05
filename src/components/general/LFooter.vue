cc3acf12a7de51813d11b85d703ef2ca9878b62c
Sergiii Iakovyshyn <iakovyshyn.s@gmail.com>
2025-08-05 18:48:30 +0300

    add social media to footer

--- src/components/general/LFooter.vue
+++ src/components/general/LFooter.vue
@@ -8,10 +8,14 @@ import LNavBarFooter from "./LNavBarFooter.vue";
     <div class="flex items-center justify-between mb-12">
       <LLogo />
       <LNavBarFooter />
-
     </div>
-    <div class="border-t-2 border-t-blue-dark mb-9">
-foo 2
+    <div class="border-t-2 border-t-blue-dark mb-9 flex justify-between items-center pt-8">
+      <div>
+        All rights reserved ® uifry.com  | Terms and conditions apply!
+      </div>
+      <div>
+        <LSocialMedia />
+      </div>
     </div>
   </div>
 </template>
---
+++ src/components/general/LSocialMedia.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const socialList = ref([
+  {
+    id: '1',
+    title: 'Facebook',
+    url: 'https://www.facebook.com',
+    icon: '/images/socialMedia/facebook.svg',
+  },
+  {
+    id: '2',
+    title: 'youtube',
+    url: 'https://www.youtube.com',
+    icon: '/images/socialMedia/youtube.svg',
+  },
+  {
+    id: '3',
+    title: 'twitter',
+    url: 'https://wwwtwitter.com',
+    icon: '/images/socialMedia/twitter.svg',
+  },
+  {
+    id: '4',
+    title: 'linkedIn',
+    url: 'https://www.linkedIn.com',
+    icon: '/images/socialMedia/linkedin.svg',
+  },
+  {
+    id: '5',
+    title: 'instagram',
+    url: 'https://www.instagram.com',
+    icon: '/images/socialMedia/instagram.svg',
+  },
+
+])
+</script>
+
+<template>
+  <div>
+    <ul class="flex gap-x-5">
+      <li v-for="item in socialList" :key="item.id">
+        <a :href="item.url" target="_blank">
+          <img :src="item.icon" :alt="item.title"/>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
